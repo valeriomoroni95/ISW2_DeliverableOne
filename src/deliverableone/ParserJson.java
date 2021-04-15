8c769930b1a5491ee4af1451fede1b9015779dbc
valeriomoroni95 <valeriomoroni95@gmail.com>
2021-04-15 10:43:28 +0200

    Solved one code smell

--- src/deliverableone/ParserJson.java
+++ src/deliverableone/ParserJson.java
@@ -1,12 +1,12 @@
 package deliverableone;
 
+import java.nio.charset.StandardCharsets;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.nio.charset.Charset;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
@@ -33,7 +33,7 @@ public class ParserJson {
 	      
 		 InputStream is = new URL(url).openStream();
 	     try {
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	        BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8.name()));
 	        String jsonText = readAll(rd);
 	        JSONArray json = new JSONArray(jsonText);
 	        return json;
@@ -45,7 +45,7 @@ public class ParserJson {
 	 public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
 	     InputStream is = new URL(url).openStream();
 	     try {
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	        BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8.name()));
 	        String jsonText = readAll(rd);
 	        JSONObject json = new JSONObject(jsonText);
 	        return json;
